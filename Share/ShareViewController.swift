0b7d60712ff2d8d71a12a4d57200d47feed10e41
Maarten van der Velden <maarten.van.der.velden@philips.com>
2016-03-14 09:13:02 +0100

    More Share functionality

--- Share/ShareViewController.swift
+++ Share/ShareViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 import Social
 
 class ShareViewController: SLComposeServiceViewController {
+    let maxCharacters = 200;
+    
+    override func presentationAnimationDidFinish() {
+        super.presentationAnimationDidFinish()
+        charactersRemaining = maxCharacters;
+        placeholder = "Text you want to share"
+    }
 
+    override func configurationItems() -> [AnyObject]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        let item = SLComposeSheetConfigurationItem()
+        item.title = "Change this"
+        
+        return [item]
+    }
+
+    override func loadPreviewView() -> UIView! {
+        return super.loadPreviewView()
+    }
+    
     override func isContentValid() -> Bool {
         // Do validation of contentText and/or NSExtensionContext attachments here
+        let characterCount = textView.text.characters.count
+        charactersRemaining = maxCharacters - characterCount
+        if charactersRemaining.integerValue < 0 {
+            return false
+        }
         return true
     }
 
@@ -22,10 +46,8 @@ class ShareViewController: SLComposeServiceViewController {
         // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
         self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
     }
-
-    override func configurationItems() -> [AnyObject]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
+    
+    override func didSelectCancel() {
+        super.didSelectCancel()
     }
-
 }
