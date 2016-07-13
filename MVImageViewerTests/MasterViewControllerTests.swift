377359c423815f347e1e4e7435ab0fa6b7c26bd5
Maarten van der Velden <maarten.van.der.velden@philips.com>
2016-07-13 23:12:53 +0200

    Initial test for master vc

---
+++ MVImageViewerTests/MasterViewControllerTests.swift
@@ -0,0 +1,19 @@
+// Created by mmvdv on 13/07/16.
+
+import Quick
+import Nimble
+@testable import MVImageViewer
+
+class MasterViewControllerSpec : QuickSpec {
+    override func spec() {
+        describe("master view controller") {
+            var subject : MasterViewController!
+            beforeEach() {
+                subject = UIViewController.loadViewController(withIdentifier: "MasterViewController", fromStoryboard: "Main") as! MasterViewController
+            }
+            it("exists") {
+                expect(subject).to(beAKindOf(MasterViewController))
+            }
+        }
+    }
+}
