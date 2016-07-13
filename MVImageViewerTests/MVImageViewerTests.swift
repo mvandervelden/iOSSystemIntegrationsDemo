aefe15bd0ae052e4479cdab3311a02c3470ad684
Maarten van der Velden <maarten.van.der.velden@philips.com>
2016-07-13 21:54:09 +0200

    Tiny view controller test

--- MVImageViewerTests/MVImageViewerTests.swift
+++ MVImageViewerTests/MVImageViewerTests.swift
@@ -1,10 +1,20 @@
 import Quick
 import Nimble
+@testable import MVImageViewer
 
-class MVImageViewerSpec: QuickSpec {
-    override func spec() {
-        it("works") {
-            expect(true).to(beTruthy())
-        }
-    }
+class DetailViewControllerSpec: QuickSpec {
+	override func spec() {
+		describe("showing the view controller") {
+            var subject : UIViewController!
+            
+            beforeEach() {
+				let storyboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+				 subject = storyboard.instantiateViewControllerWithIdentifier("DetailViewController")
+			}
+			
+			it("shows the detail description") {
+				expect(subject).to(beAKindOf(DetailViewController))
+			}
+		}
+	}
 }
